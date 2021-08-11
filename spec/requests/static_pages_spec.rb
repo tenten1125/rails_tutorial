b87cf3bc5c87cb86b5240d00f876744dc6b5d0cc
yoshito <yoshito5211@gmail.com>
2021-08-11 13:41:46 +0900

    aboutページを作成し、テストがパスすることを確認

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
+RSpec.describe 'StaticPages', type: :request do
+  describe 'GET /home' do
+    it 'homeページが表示される' do
+      get(static_pages_home_path)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
+  describe 'GET /help' do
+    it 'helpページが表示される' do
+      get(static_pages_help_path)
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'Get #about' do
+    it 'aboutページが表示される' do
+      get(static_pages_about_path)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
+　
