ca94811ffa9dcb97ff97f79ad9c9393fac270219
yoshito <yoshito5211@gmail.com>
2021-08-11 20:11:11 +0900

    各ルーティングを名前付きルートに変更

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe 'StaticPages', type: :system do
 
   describe 'GET /home' do
     it 'タイトルタブに基本タイトルのみ表示される' do
-      visit static_pages_home_path
+      visit root_path
       expect(page).to have_title @base_title.to_s
     end
   end
 
   describe 'GET /help' do
     it 'helpページが表示される' do
-      visit static_pages_help_path
+      visit help_path
       expect(page).to have_title "Help | #{@base_title}"
     end
   end
 
   describe 'Get #about' do
     it 'aboutページが表示される' do
-      visit static_pages_about_path
+      visit about_path
       expect(page).to have_title "About | #{@base_title}"
     end
   end
 
   describe 'Get #contact' do
     it '適切なタイトルが表示される' do
-      visit static_pages_contact_path
+      visit contact_path
       expect(page).to have_title "Contact | #{@base_title}"
     end
   end
