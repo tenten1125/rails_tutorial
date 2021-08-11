f9d199f0db7ed848db4eaab90473ab2dbd5344fa
yoshito <yoshito5211@gmail.com>
2021-08-11 14:47:11 +0900

    homeページをルートURLに設定

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
+  describe '/' do
+    it 'homeページが表示される' do
+      get(root_path)
+      expect(response).to have_http_status(:ok)
+    end
+  end
   describe 'GET /home' do
     it 'homeページが表示される' do
       get(static_pages_home_path)
